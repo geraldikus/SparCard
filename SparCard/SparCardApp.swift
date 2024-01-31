d5b65adde07b2b64330f51a36cdb831a50b79b67
Anton Kasian <34170533+AntonKasian@users.noreply.github.com>
2024-01-31 10:33:41 +0400

    Initial commit

---
+++ SparCard/SparCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparCardApp.swift
+//  SparCard
+//
+//  Created by Anton on 31.01.24.
+//
+
+import SwiftUI
+
+@main
+struct SparCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
